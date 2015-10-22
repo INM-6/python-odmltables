fda818fb8c701e2c70602d08ddfff5e57c997a06
Jana Pick <j.pick@fz-juelich.de>
2015-10-22 09:17:42 +0200

    added init and setup changed

--- setup.py
+++ setup.py
@@ -3,24 +3,25 @@
 from setuptools import setup
 
 long_description = open("README.md").read()
-install_requires = ['',
-                    '']
+install_requires = ['xlrd >= 0.9.3',
+                    'xlwt >= 1.0.0']
+
 extras_require = {'docs': ['sphinx>=1.2.2'],
                   'odml': ['python-odml>=0.1.0']}
 
 setup(
     name="python-odmlviz",
     version='0.1.0',
-    packages=['', ''],
+    packages=['odmlviz', 'odmlviz.tests'],
     install_requires=install_requires,
     extras_require=extras_require,
 
-    author="",
-    author_email="",
+    author="Jana Pick",
+    author_email="j.pick@fz-juelich.de",
     description="",
     long_description=long_description,
     license="BSD",
-    url='github.com',
+    url='https://github.com/j-pick/python-odmlviz',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
