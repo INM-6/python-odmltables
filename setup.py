19479679bb1603c0c46a81012c05e49b0468b3a2
Michael Denker <m.denker@fz-juelich.de>
2016-07-19 16:20:34 +0200

    Changed setup.py to install correct deps including odml

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@
 
 from setuptools import setup
 
-long_description = open("README.md").read()
-install_requires = ['xlrd >= 0.9.3',
-                    'xlwt >= 1.0.0']
+long_description = open("README.rst").read()
+install_requires = ['xlrd >= 0.9.4',
+                    'xlwt >= 1.0.0',
+                    'numpy >= 1.8.2',
+                    'quantities >= 0.10.1',
+                    'odml >= 1.1']
 
-extras_require = {'docs': ['sphinx>=1.2.2'],
-                  'odml': ['python-odml>=0.1.0']}
+extras_require = {'docs': ['numpydoc>=0.5',
+                           'sphinx>=1.2.2'],
+                  'tests': ['nose>=1.3.3']}
+
+dependency_links = [
+    'http://github.com/G-Node/python-odml/tarball/master#egg=odml-1.1']
 
 setup(
     name="python-odmltables",
@@ -16,9 +23,10 @@ setup(
     package_data={'odmltables': ['wizard/graphics/*']},
     install_requires=install_requires,
     extras_require=extras_require,
+    dependency_links=dependency_links,
 
-    author="Jana Pick",
-    author_email="j.pick@fz-juelich.de",
+    author="odML-tables authors and contributors",
+    author_email="j.sprenger@fz-juelich.de",
     description="",
     long_description=long_description,
     license="BSD",
