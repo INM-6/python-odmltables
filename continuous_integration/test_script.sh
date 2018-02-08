88fc2de36fab28df6ea40f8a5c2eb8dc38fd400d
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2018-02-08 16:49:41 +0100

    removed print scipy

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -10,7 +10,6 @@ set -e
 
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
-python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
 if [[ "$COVERAGE" == "true" ]]; then
     if [[ "$MPI" == "true" ]]; then
