c45b30522277c6e0b048ef4a503b039c3542eb8f
Julia Sprenger <julia.sprenger@rwth-aachen.de>
2018-06-01 18:51:22 +0200

    [1.0] add reference main classes at top level of odmltables

--- odmltables/__init__.py
+++ odmltables/__init__.py
@@ -1,1 +1,19 @@
 VERSION = "1.0.0"
+
+
+# -*- coding: utf-8 -*-
+"""
+:mod:`odmltables` provides classes for manipulation and conversion of odML files.
+
+Classes:
+.. autoclass:: odmltables.odml_table.OdmlTable
+.. autoclass:: odmltables.odml_csv_table.OdmlCsvTable
+.. autoclass:: odmltables.odml_xls_table.OdmlXlsTable
+.. autoclass:: odmltables.compare_section_csv_table.CompareSectionCsvTable
+"""
+
+
+from odmltables.odml_table import OdmlTable
+from odmltables.odml_csv_table import OdmlCsvTable
+from odmltables.odml_xls_table import OdmlXlsTable
+from odmltables.compare_section_csv_table import CompareSectionCsvTable
